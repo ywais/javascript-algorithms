ba88bb0df48fb98e553a3f038d4ae05e569beb83
ywais <yuvalwais@gmail.com>
2020-09-22 02:13:26 +0300

    implement priority queue

--- src/data-structures/priority-queue/PriorityQueue.js
+++ src/data-structures/priority-queue/PriorityQueue.js
@@ -1,98 +1,57 @@
-import MinHeap from '../heap/MinHeap';
-import Comparator from '../../utils/comparator/Comparator';
-
-// It is the same as min heap except that when comparing two elements
-// we take into account its priority instead of the element's value.
-export default class PriorityQueue extends MinHeap {
+export default class PriorityQueue{
   constructor() {
-    // Call MinHip constructor first.
-    super();
-
-    // Setup priorities map.
-    this.priorities = new Map();
-
-    // Use custom comparator for heap elements that will take element priority
-    // instead of element value into account.
-    this.compare = new Comparator(this.comparePriority.bind(this));
+    this.collection = [];
   }
 
-  /**
-   * Add item to the priority queue.
-   * @param {*} item - item we're going to add to the queue.
-   * @param {number} [priority] - items priority.
-   * @return {PriorityQueue}
-   */
-  add(item, priority = 0) {
-    this.priorities.set(item, priority);
-    super.add(item);
-    return this;
+  isEmpty() {
+    return (this.collection.length === 0);
   }
 
-  /**
-   * Remove item from priority queue.
-   * @param {*} item - item we're going to remove.
-   * @param {Comparator} [customFindingComparator] - custom function for finding the item to remove
-   * @return {PriorityQueue}
-   */
-  remove(item, customFindingComparator) {
-    super.remove(item, customFindingComparator);
-    this.priorities.delete(item);
-    return this;
-  }
-
-  /**
-   * Change priority of the item in a queue.
-   * @param {*} item - item we're going to re-prioritize.
-   * @param {number} priority - new item's priority.
-   * @return {PriorityQueue}
-   */
-  changePriority(item, priority) {
-    this.remove(item, new Comparator(this.compareValue));
-    this.add(item, priority);
-    return this;
-  }
-
-  /**
-   * Find item by ite value.
-   * @param {*} item
-   * @return {Number[]}
-   */
-  findByValue(item) {
-    return this.find(item, new Comparator(this.compareValue));
-  }
-
-  /**
-   * Check if item already exists in a queue.
-   * @param {*} item
-   * @return {boolean}
-   */
-  hasValue(item) {
-    return this.findByValue(item).length > 0;
-  }
-
-  /**
-   * Compares priorities of two items.
-   * @param {*} a
-   * @param {*} b
-   * @return {number}
-   */
-  comparePriority(a, b) {
-    if (this.priorities.get(a) === this.priorities.get(b)) {
-      return 0;
+  add(value, priority) {
+    if(this.isEmpty()) {
+      return (this.collection = [[value, priority]]);
     }
-    return this.priorities.get(a) < this.priorities.get(b) ? -1 : 1;
+    for (let i = 0; i < this.collection.length; i++) {
+      if (this.collection[i][1] <= priority) {
+        this.collection.splice(i, 0, [value, priority]);
+        return (this.collection);
+      }
+    }
+    this.collection.push([value, priority]);
+    return (this.collection);
   }
 
-  /**
-   * Compares values of two items.
-   * @param {*} a
-   * @param {*} b
-   * @return {number}
-   */
-  compareValue(a, b) {
-    if (a === b) {
-      return 0;
+  peek() {
+    return (this.isEmpty() ? null : this.collection[this.collection.length - 1][0]);
+  }
+
+  poll() {
+    return (this.isEmpty() ? null : this.collection.pop()[0]);
+  }
+
+  changePriority(value, priority) {
+    if(this.isEmpty()) {
+      return (null);
     }
-    return a < b ? -1 : 1;
+    for (let i = 0; i < this.collection.length; i++) {
+      if (this.collection[i][0] === value) {
+        this.collection.splice(i, 1);
+        this.add(value, priority);
+        return (this.collection);
+      }
+    }
+    return (null);
+  }
+
+  hasValue(value) {
+    if(this.isEmpty()) {
+      return (false);
+    }
+    for (let i = 0; i < this.collection.length; i++) {
+      if (this.collection[i][0] === value) {
+        return (true);
+      }
+    }
+    return (false);
   }
 }
